2b6aaffd71b2a80060e03f038c2b9fd5671f9e9a
AAvilan <andres.avilan@nurun.com>
2020-04-28 08:44:49 -0400

    Outputting a List of Recipes with ngFor - This is another take by using property binding for the src property of img html element and also using the string interpolation for the alt property. Added another test dummy line to see the result of more than one and a hr html element between the button and the list to have more space.

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -11,6 +11,7 @@ export class RecipeListComponent implements OnInit {
 
   constructor() {
     this.recipes.push(new Recipe("A Test Recipe", "This is a test.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"));
+    this.recipes.push(new Recipe("A 2nd Test Recipe", "This is another test.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"));
   }
 
   ngOnInit(): void {
