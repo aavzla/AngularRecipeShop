d44ba97378291c88bee4bb580bcd4f143ed51aaa
AAvilan <andres.avilan@nurun.com>
2020-05-20 15:52:57 -0400

    Adding Ingredients to Recipes - Add the ingredients to the recipe model and fill and show the recipes with ingredients.

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,13 +1,30 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   private recipes: Recipe[] = [];
   recipeSelected: EventEmitter<Recipe>;
 
   constructor() {
-    this.recipes.push(new Recipe("A Test Recipe", "This is a test.", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"));
-    this.recipes.push(new Recipe("A 2nd Test Recipe", "This is another test.", "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"));
+    this.recipes.push(new Recipe(
+      "Tasty Schnitzel",
+      "A super-tasty Schnitzel - just awesome!",
+      "https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG",
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ));
+    this.recipes.push(new Recipe(
+      "Big Fat Burger",
+      "What else you need to say?",
+      "https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg",
+      [
+        new Ingredient("Buns", 2),
+        new Ingredient("Meat", 1)
+      ]
+    ));
     this.recipeSelected = new EventEmitter<Recipe>();
   }
 
