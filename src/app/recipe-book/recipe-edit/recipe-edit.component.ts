f70e6d016a1914733785a46825b4f686fc76ab55
AAvilan <andres.avilan@nurun.com>
2020-06-03 12:21:30 -0400

    Retrieving Route Parameters - Setup routing for the edit recipe component for new and edit recipes and also make buttons new and edit  recipe dropdown link work with routing.

--- src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode: boolean = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        //console.log(this.constructor.name + ' - id stored value is ' + this.id);
+        //console.log(this.constructor.name + ' - editMode stored value is ' + this.editMode);
+        this.editMode = params['id'] != null;
+        if (this.editMode) {
+          this.id = +params['id'];
+        }
+        //console.log(this.constructor.name + ' - editMode value is ' + this.editMode);
+        //console.log(this.constructor.name + ' - id value is ' + this.id);
+      }
+    );
   }
 
 }
