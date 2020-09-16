2098afa910bd73cbbf448730accfc6cd6b18a534
AAvilan <andres.avilan@nurun.com>
2020-09-16 13:29:19 -0400

    Creating the Form for Editing Recipes - Creating the form at the typescript file for the new and edit of a recipe and using console logs for testing. This does not include the ingredients collection that will be treated later.

--- src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -8,9 +11,15 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class RecipeEditComponent implements OnInit {
   id: number;
-  editMode: boolean = false;
+  editMode: boolean;
 
-  constructor(private route: ActivatedRoute) { }
+  //The recipe Form container to have access to the form.
+  recipeForm: FormGroup
+
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) {
+    this.editMode = false;
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe(
@@ -23,8 +32,30 @@ export class RecipeEditComponent implements OnInit {
         }
         //console.log(this.constructor.name + ' - editMode value is ' + this.editMode);
         //console.log(this.constructor.name + ' - id value is ' + this.id);
+        this.initForm();
+        console.log(this.constructor.name + ' - the recipe name is ' + (this.recipeForm.value.name === '' ? 'empty' : this.recipeForm.value.name));
+        console.log(this.constructor.name + ' - the recipe description is ' + (this.recipeForm.value.description === '' ? 'empty' : this.recipeForm.value.description));
+        console.log(this.constructor.name + ' - the recipe imagePath is ' + (this.recipeForm.value.imagePath === '' ? 'empty' : this.recipeForm.value.imagePath));
       }
     );
   }
 
+  private initForm() {
+    let recipeName = '';
+    let recipeDescription = '';
+    let recipeImagePath = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeDescription = recipe.description;
+      recipeImagePath = recipe.imagePath;
+    }
+
+    this.recipeForm = new FormGroup({
+      name : new FormControl(recipeName),
+      description : new FormControl(recipeDescription),
+      imagePath : new FormControl(recipeImagePath)
+    });
+  }
 }
