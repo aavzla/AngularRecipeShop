40216c731c6c0e633a4f664c660c8ae2c47c76bd
AAvilan <andres.avilan@nurun.com>
2020-09-17 14:23:08 -0400

    Providing the Recipe Service Correctly - Clean up the transfer of the recipe service from the recipe book component to the app component.

--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 //import { Recipe } from './recipe.model';
-import { RecipeService } from './recipe.service';
+//import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipe-book',
@@ -11,7 +11,7 @@ export class RecipeBookComponent implements OnInit {
   //recipeSelected: Recipe;
 
   constructor(
-    private recipeService: RecipeService
+    //private recipeService: RecipeService
   ) { }
 
   ngOnInit(): void {
