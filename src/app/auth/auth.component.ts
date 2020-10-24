aed619e942fd6d3bb3eae2efcfc3a44a1da55f02
AAvilan <andres.avilan@nurun.com>
2020-10-24 17:47:01 -0400

    Switching Between Auth Modes - Handling the status of the template for Login or Signup with a property and ternary expressions.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  isLoginMode: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.isLoginMode = true;
   }
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 }
