909dd25f7d0ef16a720204962197f737f84026b3
AAvilan <andres.avilan@nurun.com>
2020-10-25 13:59:21 -0400

    Sending the Signup Request - Add the signup logic and send the http request to the Backend API to signup users.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AuthService } from './auth.service';
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -9,7 +11,7 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent implements OnInit {
   isLoginMode: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
     this.isLoginMode = true;
@@ -20,7 +22,31 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(this.constructor.name + ' - The form values.', form.value);
+    //console.log(this.constructor.name + ' - The form values.', form.value);
+
+    //This is a validation in case the html is malicious manipulated.
+    //In general, it should not be triggered, because we disabled the button in case it is invalid.
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+      //Login mode.
+
+    } else {
+      //Sign up mode.
+      this.authService.signup(email, password)
+        .subscribe(responseData => {
+          console.log(this.constructor.name + ' - Signup response.', responseData);
+        }, error => {
+          console.log(this.constructor.name + ' - Signup Error.', error);
+        });
+
+    }
+
     form.reset();
   }
 }
